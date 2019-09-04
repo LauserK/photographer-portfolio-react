372e634853efbf41c74d41706ef52cf801d77a2f
Kildare Lauser <kildarealauser@gmail.com>
2019-09-04 12:34:17 -0400

    Updates

---
+++ precache-manifest.b4b789f3f3973235580e336dcf18b47e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e995068b35ba986915ad152fb4c58d2d",
+    "url": "/photographer-portfolio-react/index.html"
+  },
+  {
+    "revision": "f8cbf815233a9f31d3e5",
+    "url": "/photographer-portfolio-react/static/css/main.991c5e9f.chunk.css"
+  },
+  {
+    "revision": "6c509e9f4811faeb4638",
+    "url": "/photographer-portfolio-react/static/js/2.897b2a5c.chunk.js"
+  },
+  {
+    "revision": "f8cbf815233a9f31d3e5",
+    "url": "/photographer-portfolio-react/static/js/main.bf3dfee1.chunk.js"
+  },
+  {
+    "revision": "52deb174e73a0c803e44",
+    "url": "/photographer-portfolio-react/static/js/runtime~main.e04b3d0f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1117d8ac49c0081831b15560398f9d68.js"
+  "/photographer-portfolio-react/precache-manifest.b4b789f3f3973235580e336dcf18b47e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/photographer-portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
